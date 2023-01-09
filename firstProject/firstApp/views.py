e1998457378e3ae235f5a7ad3007589c05092371
Huw Barber <huwbarber@gmail.com>
2023-01-09 16:48:12 +0000

    Created first apps

--- firstProject/firstApp/views.py
+++ firstProject/firstApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
 # Create your views here.
+def display(request):
+    return HttpResponse("<h1>My First Django App!</h1>")
+
+def displayDatetime(request):
+    return HttpResponse(f"Current datetime: {datetime.datetime.now()}")
